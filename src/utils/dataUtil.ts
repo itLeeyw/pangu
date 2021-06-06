2d78e8de5e4f357675dda6d470c4913ccafcb1f3
liweihao02 <liweihao02@qianxin.com>
2021-06-06 17:27:13 +0800

    feat: 项目树构建完成

---
+++ src/utils/dataUtil.ts
@@ -0,0 +1,21 @@
+/**
+ * 将 b 数组中指定数据清除
+ * todo 支持 b 数组中的正则 & 缺省语法
+ * @param a 
+ * @param b 
+ */
+ function extArrayData(a: any[], b: any): any[] {
+  let i = 0;
+  a.forEach(file => {
+    const isExt = b[file];
+    if (isExt) {
+      a.splice(i, 1);
+    }
+    i++;
+  });
+  return a;
+}
+
+export {
+  extArrayData
+}
