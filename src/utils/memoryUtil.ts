bbfcc258d45c1fdc401dc434c71e3af514da10e3
liweihao02 <liweihao02@qianxin.com>
2021-06-06 20:35:18 +0800

    feat: 修改memoryUtil文件名

---
+++ src/utils/memoryUtil.ts
@@ -0,0 +1,26 @@
+import { NATIVE_TYPE } from '../configs/configs';
+/** 深克隆
+ * 
+ * @param O 
+ * @returns 
+ */
+function deepClone(O: any): object {
+  const toStr = Object.prototype.toString;
+  if (toStr.call(O) === NATIVE_TYPE.undefined)  return O;
+  if (toStr.call(O) === NATIVE_TYPE.null)       return O;
+  if (toStr.call(O) === NATIVE_TYPE.date)       return new Date(O);
+  if (toStr.call(O) === NATIVE_TYPE.regexp)     return new RegExp(O);
+  if (toStr.call(O) !== NATIVE_TYPE.object)     return O;
+
+  let target = new O.constructor();
+  for (let k in O) {
+    if ((O as {}).hasOwnProperty(k)) {
+      target[k] = deepClone(O[k]);
+    }
+  }
+  return target;
+}
+
+export {
+  deepClone
+}
