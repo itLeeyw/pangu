e3dec358439046bd33ac4764cd7f5e200f424b96
liweihao02 <liweihao02@qianxin.com>
2021-06-06 18:48:38 +0800

    feat: 编写深拷贝结构 & 设置基础类型常量

---
+++ src/configs/nativeConfig.ts
@@ -0,0 +1,15 @@
+const NATIVE_TYPE = {
+  'undefined':  '[object Undefined]',
+  'null':       '[object Null]',
+  'boolean':    '[object Boolean]',
+  'number':     '[object Number]',
+  'string':     '[object String]',
+  'symbol':     '[object Symbol]',
+  'object':     '[object Object]',
+  'function':   '[object Function]',
+  'bigint':     '[object BigInt]',
+}
+
+export {
+  NATIVE_TYPE
+}
--- src/utils/memory.ts
+++ src/utils/memory.ts
@@ -1,7 +1,15 @@
+import { NATIVE_TYPE } from '../configs/configs';
+function deepClone<T>(O: T, t?: any): T { // 暂时使用 any 代替... todo 改造接口 ---> 类
+  let target = t ? t : {};
+  const toStr = Object.prototype.toString;
+  for (let k in O) {
+    if ((O as {}).hasOwnProperty(k)) {
+      if (toStr.call(O[k]) === NATIVE_TYPE.object) deepClone(O[k]);
 
-function deepClone(o: object): object {
-  
-  return {};
+      target[k] = O[k];
+    }
+  }
+  return target;
 }
 
 export {
